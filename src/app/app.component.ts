0e62ceab359b5c4837008d757e1188eada5d6a88
momoasr <monia_akele@yahoo.fr>
2023-09-25 22:42:28 -0400

    second change for the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Monia Application!!!';
+  title = 'Monia Application!!! from my  first branch';
 }
